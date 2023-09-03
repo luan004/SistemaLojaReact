20fdeafff613a39a90dc87f9b5ca14c4b6cf8caf
Luan Gabriel <luan.gabriel.ifpr@gmail.com>
2023-09-02 21:40:43 -0300

    02-09

--- web/src/views/cadastros/CadPessoa.jsx
+++ web/src/views/cadastros/CadPessoa.jsx
@@ -5,7 +5,6 @@ import '../../style/CadPessoa.css';
 function CadPessoa() {
   const [listar, setListar] = useState(true);
   const [incluir, setIncluir] = useState(false);
-
   function showListar() {
     setListar(true);
     setIncluir(false);
@@ -15,6 +14,16 @@ function CadPessoa() {
     setListar(false);
     setIncluir(true);
   }
+
+  function cancel() {
+    const form = document.getElementById('form');
+    form.reset();
+  }
+
+  function submit() {
+    
+  }
+
 
   return (
     <div className="viewload">
@@ -62,10 +71,62 @@ function CadPessoa() {
           </table>
         </div>
         <div className={`incluir ${incluir ? "active" : ""}`}>
-          <form className="form">
-            <div className="input-group">
-              <label htmlFor="nome">Nome</label>
-              <input type="text" id="nome" name="nome" />
+          <form className="form" id="form">
+            <div className="input-row">
+              <div className="input" style={{width:"100px"}}>
+                <label htmlFor="cod">Código</label>
+                <input type="number" id="cod" name="cod" />
+              </div>
+              <div className="input">
+                <label htmlFor="nome">Nome</label>
+                <input type="text" id="nome" name="nome" />
+              </div>
+            </div>
+            <div className="input-row">
+              <div className="input">
+                <label htmlFor="cidade">Cidade</label>
+                <select name="cidade">
+                  <option value="pvai">Cidade</option>
+                </select>
+              </div>
+              <div className="input">
+                <label htmlFor="bairro">Bairro</label>
+                <select name="bairro">
+                  <option value="pvai">Bairro</option>
+                </select>
+              </div>
+              <div className="input" style={{width:"200px"}}>
+                <label htmlFor="cep">CEP</label>
+                <input type="text" id="nome" name="cep" />
+              </div>
+            </div>
+            <div className="input-row"> 
+              <div className="input">
+                <label htmlFor="endereco">Endereço</label>
+                <input type="text" id="endereco" name="endereco" />
+              </div>
+              <div className="input" style={{width:"200px"}}>
+                <label htmlFor="numero">Número</label>
+                <input type="number" id="numero" name="numero" />
+              </div>
+              <div className="input">
+                <label htmlFor="complemento">Complemento</label>
+                <input type="text" id="complemento" name="complemento" />
+              </div>
+            </div>
+            <div className="input-row">
+              <div className="input">
+                <label htmlFor="telefone">Telefone</label>
+                <input type="text" id="telefone" name="telefone" />
+              </div>
+              <div className="input">
+                <label htmlFor="email">E-Mail</label>
+                <input type="email" id="email" name="email" />
+              </div>
+            </div>
+            <div className="input-row bottom">
+              <button id="cadPessoaSubmit" type="button" onClick={submit} className="btn green">Salvar</button>
+              <button id="cadPessoaCancel" type="button" onClick={cancel} className="btn red">Cancelar</button>
             </div>
           </form>
         </div>
