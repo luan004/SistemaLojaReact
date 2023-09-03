9ea464997b23111f7ff878432f9a0f495fd06c87
Luan Gabriel <luan.gabriel.ifpr@gmail.com>
2023-09-03 12:04:05 -0300

    03-09

--- api/models/Pessoa.mjs
+++ api/models/Pessoa.mjs
@@ -1,3 +1,5 @@
+import query from '../Data.mjs';
+
 export default class Pessoa {
     constructor(id, nome, cidade_fk, bairro_fk, cep, endereco, numero, complemento, telefone, email) {
         this.id = id;
@@ -12,23 +14,31 @@ export default class Pessoa {
         this.email = email;
     }
 
-    create(ifSuccess) {
-        query(`INSERT INTO pessoas VALUES (null, "${this.nome}", "${this.cidade_fk}", "${this.bairro_fk}", "${this.cep}", "${this.endereco}", "${this.numero}", "${this.complemento}", "${this.telefone}", "${this.email}")`, function(result) {
-            if (result) {
-                ifSuccess(result);
-            }
+    create() {
+        return new Promise((resolve, reject) => {
+            query(`INSERT INTO pessoas VALUES (null, "${this.nome}", "${this.cidade_fk}", "${this.bairro_fk}", "${this.cep}", "${this.endereco}", "${this.numero}", "${this.complemento}", "${this.telefone}", "${this.email}")`, function(result) {
+                if (result) {
+                    resolve(result);
+                } else {
+                    reject();
+                }
+            });
         });
     }
 
-    delete(ifSuccess) {
-        query(`DELETE FROM pessoas WHERE id = ${this.id}`, function(result) {
-            if (result) {
-                ifSuccess(result);
-            }
+    delete() {
+        return new Promise((resolve, reject) => {
+            query(`DELETE FROM pessoas WHERE id = ${this.id}`, function(result) {
+                if (result) {
+                    resolve(result);
+                } else {
+                    reject();
+                }
+            });
         });
     }
 
-    static async getAllPeople() {
+    static async get() {
         return new Promise((resolve, reject) => {
             query('SELECT * FROM pessoas', function(result) {
                 if (result) {
@@ -39,23 +49,16 @@ export default class Pessoa {
             });
         });
     }
-}
 
-/* 
-SQL
-CREATE TABLE pessoas (
-    id INT NOT NULL AUTO_INCREMENT,
-    nome VARCHAR(255) NOT NULL,
-    cidade_fk INT NOT NULL,
-    bairro_fk INT NOT NULL,
-    cep VARCHAR(255) NOT NULL,
-    endereco VARCHAR(255) NOT NULL,
-    numero VARCHAR(255) NOT NULL,
-    complemento VARCHAR(255) NOT NULL,
-    telefone VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (cidade_fk) REFERENCES cidades(id),
-    FOREIGN KEY (bairro_fk) REFERENCES bairros(id)
-);
-*/
+    update() {
+        return new Promise((resolve, reject) => {
+            query(`UPDATE pessoas SET nome = "${this.nome}", cidade_fk = "${this.cidade_fk}", bairro_fk = "${this.bairro_fk}", cep = "${this.cep}", endereco = "${this.endereco}", numero = "${this.numero}", complemento = "${this.complemento}", telefone = "${this.telefone}", email = "${this.email}" WHERE id = ${this.id}`, function(result) {
+                if (result) {
+                    resolve(result);
+                } else {
+                    reject();
+                }
+            }); 
+        });
+    }
+}
--- web/src/views/cadastros/CadPessoa.jsx
+++ web/src/views/cadastros/CadPessoa.jsx
@@ -1,10 +1,20 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import Modal from "../../components/Modal";
 import '../../style/Cad.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash, faPenToSquare, faFloppyDisk, faXmark } from '@fortawesome/free-solid-svg-icons'
 
 function CadPessoa() {
   const [listar, setListar] = useState(true);
   const [incluir, setIncluir] = useState(false);
+  const [itens, setItem] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [editMode, setEditMode] = useState(false);
+  const [editingItem, setEditingItem] = useState(null);
+
+  const [cidades, setCidades] = useState([]);
+  const [bairros, setBairros] = useState([]);
+
   function showListar() {
     setListar(true);
     setIncluir(false);
@@ -15,20 +25,149 @@ function CadPessoa() {
     setIncluir(true);
   }
 
+  function openModal() {
+    setShowModal(true);
+  };
+
+  function editItem(item) {
+    setEditMode(true);
+    setEditingItem(item);
+    showIncluir();
+    
+    document.getElementById('cod').value = item.id;
+    document.getElementById('nome').value = item.nome;
+    document.getElementById('cidade').value = item.cidade;
+    document.getElementById('bairro').value = item.bairro;
+    document.getElementById('cep').value = item.cep;
+    document.getElementById('endereco').value = item.endereco;
+    document.getElementById('numero').value = item.numero;
+    document.getElementById('complemento').value = item.complemento;
+    document.getElementById('telefone').value = item.telefone;
+    document.getElementById('email').value = item.email;
+  };
+
   function cancel() {
     const form = document.getElementById('form');
     form.reset();
+    setEditMode(false);
   }
 
-  function submit() {
-    
-  }
+  function closeModal() {
+    setShowModal(false);
+  };
 
+  useEffect(() => { // Executar loadLista quando o componente for montado
+    if (listar) {
+      loadLista();
+    }
+  }, [listar]);
+
+  useEffect(() => { // Executar loadComboBoxCidade quando o componente for montado
+    if (incluir) {
+      loadComboBoxCidade();
+      loadComboBoxBairro();
+    }
+  }, [incluir]);
+
+  async function loadComboBoxCidade () {
+    try {
+      const response = await fetch("http://localhost:3001/api/cidades");
+      if (!response.ok) {
+        throw new Error("Erro ao buscar dados da API");
+      }
+      const data = await response.json();
+      setCidades(data);
+    } catch (error) {
+      console.error("Erro ao buscar dados da API:", error);
+    }
+  };
+
+  async function loadComboBoxBairro() {
+    try {
+      const response = await fetch("http://localhost:3001/api/bairros");
+      if (!response.ok) {
+        throw new Error("Erro ao buscar dados da API");
+      }
+      const data = await response.json();
+      setBairros(data);
+    } catch (error) {
+      console.error("Erro ao buscar dados da API:", error);
+    }
+  };
+
+  async function loadLista() { // LISTAR ITEMS
+    try {
+      const response = await fetch("http://localhost:3001/api/cidades");
+      if (!response.ok) {
+        throw new Error("Erro ao buscar dados da API");
+      }
+      const data = await response.json();
+      setItem(data);
+    } catch (error) {
+      console.error("Erro ao buscar dados da API:", error);
+    }
+  };
+
+  function submit() { // CRIAR ITEM OU EDITAR ITEM
+    if (editMode) {
+      const edited = {
+        id: document.getElementById('cod').value,
+        nome: document.getElementById('nome').value,
+        cidade: document.getElementById('cidade').value,
+        bairro: document.getElementById('bairro').value,
+        cep: document.getElementById('cep').value,
+        endereco: document.getElementById('endereco').value,
+        numero: document.getElementById('numero').value,
+        complemento: document.getElementById('complemento').value,
+        telefone: document.getElementById('telefone').value,
+        email: document.getElementById('email').value
+      };
+  
+      fetch(`http://localhost:3001/api/cidades/${editingItem.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(edited),
+      })
+    } else {
+      const newItem = {
+        nome: document.getElementById('nome').value,
+        cidade: document.getElementById('cidade').value,
+        bairro: document.getElementById('bairro').value,
+        cep: document.getElementById('cep').value,
+        endereco: document.getElementById('endereco').value,
+        numero: document.getElementById('numero').value,
+        complemento: document.getElementById('complemento').value,
+        telefone: document.getElementById('telefone').value,
+        email: document.getElementById('email').value
+      };
+  
+      fetch("http://localhost:3001/api/cidades", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newItem),
+      })
+    }
+    cancel();
+  }
+  
+  function confirmModal(item) { // EXCLUIR ITEM
+    fetch(`http://localhost:3001/api/cidades/${item.id}`, {
+      method: "DELETE",
+    })
+    .then((data) => {
+      loadLista();
+    })
+    setShowModal(false);
+  };
 
   return (
     <div className="viewload">
       <div className="title">
-        <span>Cadastro de Pessoa</span>
+        <span>Cadastro de Pessoas</span>
       </div>
       <div className="window">
         <div className="tab-menu">
@@ -40,45 +179,52 @@ function CadPessoa() {
             <thead>
                 <tr>
                   <th>Código</th>
-                  <th>Nome</th>
+                  <th>Nome da Pessoa</th>
                   <th>Cidade</th>
                   <th>Telefone</th>
                   <th>Ações</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>João</td>
-                  <td>Curitiba</td>
-                  <td>(41) 99999-9999</td>
+              {itens.map((item) => (
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>{item.nome}</td>
+                  <td>{item.cidade}</td>
+                  <td>{item.telefone}</td>
                   <td>
-                    <button className="btn-action edit">Editar</button>
-                    <button className="btn-action remove">Excluir</button>
+                    <button className="btn-action edit" onClick={() => editItem(item)}>
+                      <FontAwesomeIcon icon={faPenToSquare} />
+                      Editar
+                    </button>
+                    <button className="btn-action remove" onClick={openModal}>
+                      <FontAwesomeIcon icon={faTrash} />
+                      Excluir
+                    </button>
+                    { showModal && (
+                      <Modal 
+                        message="Deseja realmente excluir o registro?"
+                        confirmText="Sim"
+                        cancelText="Não"
+                        onConfirm={() => confirmModal(item)}
+                        onClose={closeModal}
+                      />
+                    )}
                   </td>
                 </tr>
-                <tr>
-                  <td>1</td>
-                  <td>João</td>
-                  <td>Curitiba</td>
-                  <td>(41) 99999-9999</td>
-                  <td>
-                    <button className="btn-action edit">Editar</button>
-                    <button className="btn-action remove">Excluir</button>
-                  </td>
-                </tr>
+              ))}
             </tbody>
           </table>
         </div>
         <div className={`incluir ${incluir ? "active" : ""}`}>
           <form className="form" id="form">
-            <div className="input-row">
+          <div className="input-row">
               <div className="input" style={{width:"100px"}}>
                 <label htmlFor="cod">Código</label>
-                <input type="number" id="cod" name="cod" />
+                <input type="number" id="cod" name="cod" disabled />
               </div>
               <div className="input">
-                <label htmlFor="nome">Nome</label>
+                <label htmlFor="nome">Nome da Pessoa</label>
                 <input type="text" id="nome" name="nome" />
               </div>
             </div>
@@ -86,13 +232,17 @@ function CadPessoa() {
               <div className="input">
                 <label htmlFor="cidade">Cidade</label>
                 <select name="cidade">
-                  <option value="pvai">Cidade</option>
+                  {cidades && cidades.map((cidade) => (
+                    <option key={cidade.id} value={cidade.id}>{cidade.nome}</option>
+                  ))}
                 </select>
               </div>
               <div className="input">
                 <label htmlFor="bairro">Bairro</label>
                 <select name="bairro">
-                  <option value="pvai">Bairro</option>
+                  {bairros && bairros.map((bairro) => (
+                    <option key={bairro.id} value={bairro.id}>{bairro.nome}</option>
+                  ))}
                 </select>
               </div>
               <div className="input" style={{width:"200px"}}>
@@ -117,7 +267,7 @@ function CadPessoa() {
             <div className="input-row">
               <div className="input">
                 <label htmlFor="telefone">Telefone</label>
-                <input type="text" id="telefone" name="telefone" />
+                <input type="tel" id="telefone" name="telefone" />
               </div>
               <div className="input">
                 <label htmlFor="email">E-Mail</label>
@@ -125,8 +275,14 @@ function CadPessoa() {
               </div>
             </div>
             <div className="input-row bottom">
-              <button id="cadPessoaSubmit" type="button" onClick={submit} className="btn green">Salvar</button>
-              <button id="cadPessoaCancel" type="button" onClick={cancel} className="btn red">Cancelar</button>
+              <button type="button" onClick={submit} className="btn green">
+                <FontAwesomeIcon icon={faFloppyDisk} />
+                Salvar
+              </button>
+              <button type="button" onClick={cancel} className="btn red">
+                <FontAwesomeIcon icon={faXmark} />
+                Cancelar
+              </button>
             </div>
           </form>
         </div>
